f43d84972684a5905a13945c8051c4fb53b7bc06
Gabriel Martinez <mart.gabriel96@gmail.com>
2023-03-11 17:40:05 -0300

    modificacion 1

--- desafio_1.js
+++ desafio_1.js
@@ -1,38 +1,83 @@
-class ProductManager
+class productManager
 {
+    idProduct = 1;
     constructor ()
     {
         this.products = [];
     }
-
-    addProduct()
+   
+    addProduct(newProduct)
     {
-        //guardar el producto en el array de products
-        let nuevoProducto = {
-            title: 'Manzana',
-            description: 'Fruta',
-            price: 50,
-            thumbnail: 'img1',
-            code: 1,
-            stock: 100
-        }
-        return `${this.products.push(nuevoProducto)} Se agrego el nuevo producto: ${this.products}`
-        //incrementar el autoID
+       
+        // if (!newProduct)
+        // {
+            //     throw new Error ("no se puede crear un producto vacio");
+            // }
+            
+            //Validacion de code.
+            if(this.products.find(p => p.code === newProduct.code))
+            {
+                throw new Error ("No se puede crear un producto con Id repetido")
+            }
+            
+            //Retornar el producto y asignarle una autoID con spread operator
+            this.products.push({...newProduct, id:this.idProduct++});
+
+            return "producto" + (this.idProduct-1) +  "agregado correctamente";
+
+
+       
     }
 
+    //Retorna la lista de los productos.
     getProducts()
     {
-        return `${this.products}`
+        return this.products;
     }
 
-    getProductsById()
+    //retorna un producto por su id.
+    getProductsById(id)
     {
-        //filtrar el producto por id
+
+        if(this.products.find(p => p.idProduct === id))
+            {
+                return this.idProduct
+            }
+            throw new Error ("No se encuentra la id ingresada")
     }
 }
 
-const ProductManager = new Product();
 
-ProductManager.addProduct({}):
-ProductManager.addProduct({}):
-ProductManager.addProduct({}):
+
+//Guardar el producto en el array de products.
+
+let newProduct1 = {
+    title: 'Manzana',
+    description: 'Fruta',
+    price: 50,
+    thumbnail: 'img1',
+    code: 1,
+    stock: 100,
+};
+let newProducto2 = {
+    title: 'Manzana',
+    description: 'Fruta',
+    price: 50,
+    thumbnail: 'img1',
+    code: 1,
+    stock: 100,
+};
+
+
+
+
+const productManager = new productManager();
+
+
+productManager.addProduct(newProduct1);
+productManager.addProduct(newProduct2);
+
+
+console.log(productManager.getProducts());
+
+productManager.getProductsById(2)
