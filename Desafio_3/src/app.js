b555daa68851e697cbd2234286f48baa222f4f5d
Gabriel Martinez <106201692+Martinez-Gabriel@users.noreply.github.com>
2023-03-22 20:47:59 -0300

    modificando desafio 3

--- Desafio_3/src/app.js
+++ Desafio_3/src/app.js
@@ -1,35 +1,43 @@
-import express from "express";
+const express = require("express");
+const ProductManager = require("./productManager");
 
 const app = express();
 
-const products = [
-    { id: 1, type: 'Azucar'},
-    { id: 2, type: 'Te'},
-    { id: 3, type: 'Harina'},
-    { id: 4, type: 'Huevo'}
-  ];
+const productManager = new ProductManager();
 
 //MIDDLEWARE REQ.QUERY
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 //GET PRODUCT POR LIMIT.
-app.get('/products', (req, res) => {
-    res.send(products)
+app.get("/products", async (req, res) => {
+  const limit = +req.query.limit;
+  let productLimit = [];
+  for (var i = 0; i < limit; i++){
+    productLimit.push(products[i]);
+  }
+  res.send(await productManager.getProducts());
 });
 
 //GET PRODUCT POR ID.
-app.get('/products/:pid', (req, res) => {
-const pid = +req.params.pid;
-const product = products.find( product => product.id === pid);
-  if(!product)
-  {
-      res.send({ error: "Product not found."});
+app.get("/products/:pid", (req, res) => {
+  const pid = +req.params.pid;
+  const product = products.find((product) => product.id === pid);
+  if (!product) {
+    res.send({ error: "Product not found." });
   }
 
   res.send(product);
-}); 
+});
 
 //CONFIGURACION DE PUERTO
 app.listen(8080, () => {
-    console.log('Servidor escuchando el puerto 8080');
+  console.log("Servidor escuchando el puerto 8080");
 });
+
+// app.get("/addProduct", async (req, res) => {
+//   res.send(await productManager.addProduct(singleProduct));
+// });
+
+
+
+
